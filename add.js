e0f536327113b4d51e6b9634a283ede96bf2b3b4
Adarsh <adarshgp96@gmail.com>
2024-05-15 16:05:09 +0530

    CI/CD for add 2 numbers

---
+++ add.js
@@ -0,0 +1,24 @@
+// // 012 cows
+// // 011 chicken
+
+
+// function cowchicken(cows, chicken){
+//     let cowstring = String(cows)
+//     while(cowstring.length < 3){
+//         cowstring = "0" + cowstring
+//     }
+//     console.log(`${cowstring} cows`);
+
+// }
+// cowchicken(7,10);
+
+
+
+// File: add.js
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  module.exports = add;
+  
